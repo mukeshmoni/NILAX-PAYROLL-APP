763941ed86993713a8baaf4de4061ba9c7471332
Moni Sandy <148899466+MoniSandy0204@users.noreply.github.com>
2024-09-20 11:20:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ API_ID = int(getenv("API_ID", "24274375"))
 API_HASH = getenv("API_HASH", "9d88affc9ab571a6695e069cef5d363b")
 # ------------------------------------------------------
 
-BOT_TOKEN = getenv("BOT_TOKEN", "7369094787:AAFLCZ6QjCi2YvYOp_T-emU7ZEWPjIA73Hw")
+BOT_TOKEN = getenv("BOT_TOKEN", "7369094787:AAE4YCFiTr5eycR7LgqEvYVrvWS02Vj5ErM")
 
 # -------------------------------------------------------
 OWNER_USERNAME = getenv("OWNER_USERNAME", "Mr_king008")
